226d07c1dfa3f7983f1273f5705404f650b7d027
Oscar Nash Tetteh - 01240596D <oscartetteh41@gmail.com>
2026-02-25 12:40:06 +0000

    WEEK 4 - FILE STORAGE

--- main.cpp
+++ main.cpp
@@ -1,59 +1,81 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 using namespace std;
 
-struct AttendanceRecord {
+class Student {
+public:
     string indexNumber;
-    string status;
-};
+    string name;
 
-vector<AttendanceRecord> records;
-
-void markAttendance() {
-    string index, status;
-
-    cout << "Enter Index Number: ";
-    cin >> index;
-
-    cout << "Enter Status (Present/Absent/Late): ";
-    cin >> status;
-
-    records.push_back({index, status});
-    cout << "Recorded.\n";
-}
-
-void showReport() {
-    int present = 0, absent = 0, late = 0;
-
-    for (int i = 0; i < records.size(); i++) {
-        if (records[i].status == "Present") present++;
-        else if (records[i].status == "Absent") absent++;
-        else if (records[i].status == "Late") late++;
+    Student(string idx, string nm) {
+        indexNumber = idx;
+        name = nm;
     }
 
-    cout << "\n--- SUMMARY ---\n";
-    cout << "Present: " << present << endl;
-    cout << "Absent : " << absent << endl;
-    cout << "Late   : " << late << endl;
+    void display() {
+        cout << indexNumber << " - " << name << endl;
+    }
+};
+
+vector<Student> students;
+
+void loadStudents() {
+    ifstream file("students.txt");
+    string index, name;
+
+    while (getline(file, index, ',') && getline(file, name)) {
+        students.push_back(Student(index, name));
+    }
+
+    file.close();
+}
+
+void saveStudents() {
+    ofstream file("students.txt");
+
+    for (int i = 0; i < students.size(); i++) {
+        file << students[i].indexNumber << "," << students[i].name << endl;
+    }
+
+    file.close();
+}
+
+void addStudent() {
+    string index, name;
+    cout << "Index: ";
+    cin >> index;
+    cin.ignore();
+    cout << "Name: ";
+    getline(cin, name);
+
+    students.push_back(Student(index, name));
+}
+
+void viewStudents() {
+    for (int i = 0; i < students.size(); i++)
+        students[i].display();
 }
 
 int main() {
-    int choice;
+    loadStudents();
 
+    int choice;
     do {
-        cout << "\n==== ATTENDANCE MENU ====\n";
-        cout << "1. Mark Attendance\n";
-        cout << "2. View Report\n";
+        cout << "\n==== FINAL SYSTEM ====\n";
+        cout << "1. Add Student\n";
+        cout << "2. View Students\n";
         cout << "0. Exit\n";
         cout << "Choice: ";
         cin >> choice;
 
         switch (choice) {
-            case 1: markAttendance(); break;
-            case 2: showReport(); break;
+            case 1: addStudent(); break;
+            case 2: viewStudents(); break;
         }
 
     } while (choice != 0);
 
+    saveStudents();
     return 0;
 }
